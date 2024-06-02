65ab01a003c3a218819aaf7790e56e347f41b511
Sheryl L <s_lawrence0@yahoo.ca>
2024-06-01 22:11:08 -0400

    Starter code added to App.jsx and index.html

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+// Import CSS file as a dependency
+// import '../styles/Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Home</h1>
+        </header>
+    );
+}
+
+export default Header;
