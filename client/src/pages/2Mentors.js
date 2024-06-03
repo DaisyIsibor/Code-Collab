5a6d05a49445d96456d2e0ceb466ce5fefec59d6
Sheryl L <s_lawrence0@yahoo.ca>
2024-06-03 17:03:28 -0400

    Header with links added to Mentors.js

--- client/src/pages/2Mentors.js
+++ client/src/pages/2Mentors.js
@@ -1,1 +1,28 @@
 import React from 'react';
+
+export default function 2Mentors() {
+    const linkStyle = { border: '5px black', padding: '10px' };
+
+    return (
+        <nav className='main-header-menu'>
+            <section
+            style={{
+                display: 'flex',
+                fontFamily: 'helvetica',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+            }}
+            >
+            <div style={linkStyle}>
+                <a href='#'>Mentors</a>
+            </div>
+            <div style={linkStyle}>
+                <a href='#'>Collaborators</a>
+            </div>
+            <div style={linkStyle}>
+                <a href='#'>Study Buddy</a>
+            </div>
+            </section>
+        </nav>
+    )
+}
--- client/src/pages/3Profile.js
+++ client/src/pages/3Profile.js
@@ -1,1 +1,7 @@
-import React from 'react';
+// import React from 'react';
+import { useState } from 'react';
+
+function Form() {
+    const [firstName, setFirstName] = useState('');
+    const[lastName]
+}
