4b98853bdb30a25c736b039957c839898c83155d
Sheryl L <s_lawrence0@yahoo.ca>
2024-06-10 14:33:35 -0400

    Work in Profile.jsx for user profile creation and updating

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -94,14 +94,24 @@ export default function Profile() {
 
 // MODULE 22 / ACTIVITY 25
 
-// Posting user profiles
-// app.post('/profile', (req, res) => {
-//     const profileData = req.body;
-//     console.log('Profile Data:', profile: profileData );
+// In server.js
 
-// Save data to database
-// res.status(201).send({ message: 'Profile successfully created!', profile: profileData });
+// let profiles = [];
+
+// app.post('/profiles', (req, res) => {
+//     const { firstname, lastname, email, username, password,bio, codingLanguages, meetingPreference, connectionHistory, reviews, role } = req.body;
+// const profileData = { id: profiles.length + 1, firstname, lastname, email, username, password,bio, codingLanguages, meetingPreference, connectionHistory, reviews, role };
+// profiles.push(profileData);
 // });
+
+
+// app.get('/profiles', (req, res) => {
+//  res.json(profiles);
+// });
+
+
+
+
 
 
 // function FormExample() {
@@ -118,170 +128,3 @@ export default function Profile() {
 //     file: yup.mixed().required(),
 //     terms: yup.bool().required().oneOf([true], 'terms must be accepted'),
 //   });
-
-//   return (
-//     <Formik
-//       validationSchema={schema}
-//       onSubmit={console.log}
-//       initialValues={{
-//         firstName: 'Mark',
-//         lastName: 'Otto',
-//         username: '',
-//         city: '',
-//         state: '',
-//         zip: '',
-//         file: null,
-//         terms: false,
-//       }}
-//     >
-//       {({ handleSubmit, handleChange, values, touched, errors }) => (
-//         <Form noValidate onSubmit={handleSubmit}>
-//           <Row className="mb-3">
-//             <Form.Group
-//               as={Col}
-//               md="4"
-//               controlId="validationFormik101"
-//               className="position-relative"
-//             >
-//               <Form.Label>First name</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 name="firstName"
-//                 value={values.firstName}
-//                 onChange={handleChange}
-//                 isValid={touched.firstName && !errors.firstName}
-//               />
-//               <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group
-//               as={Col}
-//               md="4"
-//               controlId="validationFormik102"
-//               className="position-relative"
-//             >
-//               <Form.Label>Last name</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 name="lastName"
-//                 value={values.lastName}
-//                 onChange={handleChange}
-//                 isValid={touched.lastName && !errors.lastName}
-//               />
-
-//               <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group as={Col} md="4" controlId="validationFormikUsername2">
-//               <Form.Label>Username</Form.Label>
-//               <InputGroup hasValidation>
-//                 <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-//                 <Form.Control
-//                   type="text"
-//                   placeholder="Username"
-//                   aria-describedby="inputGroupPrepend"
-//                   name="username"
-//                   value={values.username}
-//                   onChange={handleChange}
-//                   isInvalid={!!errors.username}
-//                 />
-//                 <Form.Control.Feedback type="invalid" tooltip>
-//                   {errors.username}
-//                 </Form.Control.Feedback>
-//               </InputGroup>
-//             </Form.Group>
-//           </Row>
-//           <Row className="mb-3">
-//             <Form.Group
-//               as={Col}
-//               md="6"
-//               controlId="validationFormik103"
-//               className="position-relative"
-//             >
-//               <Form.Label>City</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="City"
-//                 name="city"
-//                 value={values.city}
-//                 onChange={handleChange}
-//                 isInvalid={!!errors.city}
-//               />
-
-//               <Form.Control.Feedback type="invalid" tooltip>
-//                 {errors.city}
-//               </Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group
-//               as={Col}
-//               md="3"
-//               controlId="validationFormik104"
-//               className="position-relative"
-//             >
-//               <Form.Label>State</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="State"
-//                 name="state"
-//                 value={values.state}
-//                 onChange={handleChange}
-//                 isInvalid={!!errors.state}
-//               />
-//               <Form.Control.Feedback type="invalid" tooltip>
-//                 {errors.state}
-//               </Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group
-//               as={Col}
-//               md="3"
-//               controlId="validationFormik105"
-//               className="position-relative"
-//             >
-//               <Form.Label>Zip</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="Zip"
-//                 name="zip"
-//                 value={values.zip}
-//                 onChange={handleChange}
-//                 isInvalid={!!errors.zip}
-//               />
-
-//               <Form.Control.Feedback type="invalid" tooltip>
-//                 {errors.zip}
-//               </Form.Control.Feedback>
-//             </Form.Group>
-//           </Row>
-//           <Form.Group className="position-relative mb-3">
-//             <Form.Label>File</Form.Label>
-//             <Form.Control
-//               type="file"
-//               required
-//               name="file"
-//               onChange={handleChange}
-//               isInvalid={!!errors.file}
-//             />
-//             <Form.Control.Feedback type="invalid" tooltip>
-//               {errors.file}
-//             </Form.Control.Feedback>
-//           </Form.Group>
-//           <Form.Group className="position-relative mb-3">
-//             <Form.Check
-//               required
-//               name="terms"
-//               label="Agree to terms and conditions"
-//               onChange={handleChange}
-//               isInvalid={!!errors.terms}
-//               feedback={errors.terms}
-//               feedbackType="invalid"
-//               id="validationFormik106"
-//               feedbackTooltip
-//             />
-//           </Form.Group>
-//           <Button type="submit">Submit form</Button>
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// }
-
-
-// export default FormExample;
