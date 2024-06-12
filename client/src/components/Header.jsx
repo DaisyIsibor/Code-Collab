48d44c5d47b440629b4246be40890951884b1c81
daisyisibor <daisyisibor8@gmail.com>
2024-06-12 03:44:16 -0400

    added more styling to code, moved log out button and also styled the chat room page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 import './Header.css';
 
-const Header = ({ loggedIn, setLoggedIn }) => {
+const Header = ({ loggedIn, setLoggedIn, handleRoleFilter }) => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
@@ -11,27 +11,27 @@ const Header = ({ loggedIn, setLoggedIn }) => {
   };
 
   return (
-    <header className='bg-info text-dark mb-4 py-3 display-flex align-center'>
+    <header className='header-container bg-info text-dark mb-4 py-3 display-flex align-center'>
       <div className='container flex-column justify-space-between-lg justify-center align-center text-center'>
         <Link className='text-light' to='/'>
-          <h1 className='m-0' style={{ fontSize: '3rem' }}>Code Collab</h1>
+          <h1 className='m-0' style={{ fontSize: '3rem' }}>Code Collab Connect</h1>
         </Link>
         <p className='m-0' style={{ fontSize: '1.75rem', fontWeight: '400' }}>Find your new programming pals!</p>
-        <div>
-          {loggedIn ? (
-            <>
-              <button className='btn btn-lg btn-light m-2' onClick={logout}>Logout</button>
+        {loggedIn ? (
+          <>
+            <div className='button-container'>
               <button className='btn btn-lg btn-light m-2' onClick={() => handleRoleFilter('Mentor')}>Mentors</button>
               <button className='btn btn-lg btn-light m-2' onClick={() => handleRoleFilter('Study Buddy')}>Study Buddies</button>
               <button className='btn btn-lg btn-light m-2' onClick={() => handleRoleFilter('Collaborator')}>Collaborators</button>
-            </>
-          ) : (
-            <>
-              <Link className='btn btn-lg btn-primary m-2' to='/login'>Login</Link>
-              <Link className='btn btn-lg btn-light m-2' to='/signup'>Signup</Link>
-            </>
-          )}
-        </div>
+            </div>
+            <button className='btn btn-logout' onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <div className='button-container'>
+            <Link className='btn btn-lg btn-primary m-2' to='/login'>Login</Link>
+            <Link className='btn btn-lg btn-light m-2' to='/signup'>Signup</Link>
+          </div>
+        )}
       </div>
     </header>
   );
--- client/src/pages/Signup/index.jsx
+++ client/src/pages/Signup/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import RegisterForm from '../../components/RegisterForm';
+
 
 const Signup = () => {
   return (
