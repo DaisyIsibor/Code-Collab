2e8e55e92c9c881e59b1e7df5e4d107d33f82b0f
daisyisibor <daisyisibor8@gmail.com>
2024-06-13 00:50:14 -0400

    added styling to home page and also installed package front awsome

--- client/src/pages/UserList/index.jsx
+++ client/src/pages/UserList/index.jsx
@@ -98,21 +98,26 @@ const UserList = () => {
       <MDBTypography tag="h6" className="header-text">
         Total Registered Users: {displayedCount}
       </MDBTypography>
+      <div style={{ marginBottom: '20px' }}>
       <input
         type="text"
         placeholder="Search users..."
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        style={{ width: '80%', height:'55px'}}
       />
+      </div>
       <MDBRow className="justify-content-center">
         {filteredUsers.map((user) => (
-          <MDBCol md="6" lg="4" key={user._id}>
+          
+          <MDBCol md="6" lg="4" >
             <MDBCard className="user-card">
               <MDBCardBody className="user-card-body text-center">
                 <MDBCardImage
                   src={user.photo ? `/images/${user.photo}` : '/default-image.jpg'}
                   alt={user.username}
-                  className="user-card-image mb-3"
+                  className="user-card-image mb-3" 
+                  style={{ height: '200px', width: '200px'}}
                   fluid
                 />
                 <MDBTypography tag="h5" className="mb-2">
@@ -126,7 +131,7 @@ const UserList = () => {
                 </MDBTypography>
                 <div className="user-card-buttons">
                   <Link to={`/users/${user._id}`}>
-                    <MDBBtn outline color="dark" rounded size="sm">
+                    <MDBBtn outline color="dark" rounded size="sm" style={{ backgroundColor: '#61dafbaa' }}>
                       Go to profile
                     </MDBBtn>
                   </Link>
